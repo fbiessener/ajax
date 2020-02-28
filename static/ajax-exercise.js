e72f814a71fa39659cd6139b4dd34331aacb3d13
Your Name <you@example.com>
2020-02-28 15:04:20 -0600

    Further Study Anonymous Inline Functions complete

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -3,79 +3,97 @@
 
 // PART 1: SHOW A FORTUNE
 
-function showFortune(evt) {
+// function showFortune(evt) {
 
-    // TODO: get the fortune and show it in the #fortune-text div
-    // $.get('/fortune', (response) => {
-    //   // .text() returned plain text
-    //   // .html() returns the html formatting for the response given by the form
-    //       $('#fortune-text').html(response); 
-    // });
+//     // TODO: get the fortune and show it in the #fortune-text div
+//     // $.get('/fortune', (response) => {
+//     //   // .text() returned plain text
+//     //   // .html() returns the html formatting for the response given by the form
+//     //       $('#fortune-text').html(response); 
+//     // });
 
-    // get refactor with load()
-    $('#fortune-text').load('/fortune');
-}
+//     // get refactor with load()
+//     $('#fortune-text').load('/fortune');
+// }
 
-$('#get-fortune-button').on('click', showFortune);
-
-
-
+// Anonymous inline function refactor
+$('#get-fortune-button').on('click', () => {
+  $('#fortune-text').load('/fortune');
+});
 
 
 // PART 2: SHOW WEATHER
 
-function showWeather(evt) {
-    evt.preventDefault();
+// function showWeather(evt) {
+//     evt.preventDefault();
 
-    let url = '/weather.json';
-    // let formData = {'zipcode': $('#zipcode-field').val()};
+//     let url = '/weather.json';
+//     // let formData = {'zipcode': $('#zipcode-field').val()};
 
-    // serialize() refactor
-    const formValues = $('#weather-form').serialize();
+//     // serialize() refactor
+//     const formValues = $('#weather-form').serialize();
 
-    // TODO: request weather with that URL and show the forecast in #weather-info
-    // replace formValues with formData to run non-serialized code block
-    $.get(url, formValues, (response) => {
-      $('#weather-info').text(response.forecast);
-    });
+//     // TODO: request weather with that URL and show the forecast in #weather-info
+//     // replace formValues with formData to run non-serialized code block
+//     $.get(url, formValues, (response) => {
+//       $('#weather-info').text(response.forecast);
+//     });
+// }
+
+// Anonymous inline function refactor
+$('#weather-form').on('submit', (evt) => {
+  evt.preventDefault();
+  let url = '/weather.json';
+  const formValues = $('#weather-form').serialize();
+  $.get(url, formValues, (response) => {
+    $('#weather-info').text(response.forecast);
+  });
+});
 
 
-}
-
-$('#weather-form').on('submit', showWeather);
 
 
+// // PART 3: ORDER MELONS
 
+// function orderMelons(evt) {
+//     evt.preventDefault();
 
-// PART 3: ORDER MELONS
-
-function orderMelons(evt) {
-    evt.preventDefault();
-
-    // TODO: show the result message after your form
-    // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+//     // TODO: show the result message after your form
+//     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
  
-    // explicitly getting each individual input from the form
-    // const formInputs = {
-    //   'qty': $('#qty-field').val(),
-    //   'melon_type': $('#melon-type-field').val()
-    // };
+//     // explicitly getting each individual input from the form
+//     // const formInputs = {
+//     //   'qty': $('#qty-field').val(),
+//     //   'melon_type': $('#melon-type-field').val()
+//     // };
 
-    // console.log(formInputs);
+//     // console.log(formInputs);
 
-    // Same code, but with serialize() instead
-    const formValues = $('#order-form').serialize();
+//     // Same code, but with serialize() instead
+//     const formValues = $('#order-form').serialize();
 
-    // change data param to formInputs to run commented out block instead of 
-    // serialize() block
-    $.post('/order-melons.json', formValues, (response) => {
-      $('#order-status').text(response.msg);
-      if (response.code === 'ERROR') {
-        $('#order-status').addClass('order-error');
-      } else {
-        $('#order-status').removeClass('order-error');
-      }
-    });
-}
+//     // change data param to formInputs to run commented out block instead of 
+//     // serialize() block
+//     $.post('/order-melons.json', formValues, (response) => {
+//       $('#order-status').text(response.msg);
+//       if (response.code === 'ERROR') {
+//         $('#order-status').addClass('order-error');
+//       } else {
+//         $('#order-status').removeClass('order-error');
+//       }
+//     });
+// }
 
-$('#order-form').on('submit', orderMelons);
+// Anonymous inline function refactor
+$('#order-form').on('submit', (evt) => {
+  evt.preventDefault();
+  const formValues = $('#order-form').serialize();
+  $.post('/order-melons.json', formValues, (response) => {
+    $('#order-status').text(response.msg);
+    if (response.code === 'ERROR') {
+      $('#order-status').addClass('order-error');
+    } else {
+      $('#order-status').removeClass('order-error');
+    }
+  });
+});
